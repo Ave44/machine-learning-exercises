9001272a06defe554d2ca57fc6c8d23d4649b389
Paweł Olszewski <monte.paw@gmail.com>
2022-03-10 20:25:01 +0100

    Rozwiązanie zadania 3

---
+++ Lab01/zad3.py
@@ -0,0 +1,45 @@
+from cProfile import label
+from matplotlib import markers
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# a)
+
+miasta = pd.read_csv('Lab01/miasta.csv')
+
+print(miasta)
+
+print(miasta.values)
+
+# b)
+
+newRow = pd.DataFrame([[2010, 460, 555, 405]], columns=['Rok', 'Gdansk', 'Poznan', 'Szczecin'])
+
+miasta = pd.concat([miasta, newRow], ignore_index=True)
+
+print(miasta)
+
+# c)
+
+miasta.plot(x='Rok',
+            y='Gdansk',
+            color='red',
+            marker='o',
+            legend=False,
+            title='Ludność w miastach Polski',
+            xlabel='Lata',
+            ylabel='Liczba ludności (w tys)')
+
+# d)
+
+plt.figure()
+plt.plot(miasta.Rok, miasta.Gdansk, label='Gdansk', marker='o', color='red')
+plt.plot(miasta.Rok, miasta.Poznan, label='Poznan', marker='o')
+plt.plot(miasta.Rok, miasta.Szczecin, label='Szczecin', marker='o', color='green')
+
+plt.legend()
+plt.title('Ludność w miastach Polski')
+plt.xlabel('Lata')
+plt.ylabel('Liczba ludności (w tys)')
+
+plt.show()

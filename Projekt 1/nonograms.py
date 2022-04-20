8b9731038e29e5468a778d2e9cc7f908c20c337a
Paweł Olszewski <monte.paw@gmail.com>
2022-04-20 15:48:04 +0200

    Ogólne usprawnienia i optymalizacje

--- Projekt 1/nonograms.py
+++ Projekt 1/nonograms.py
@@ -14,7 +14,7 @@ horizontalM20 = [[3],[2,1],[5],[2,1,2],[2,2,3],[3,3,1],[9,3],[4,4,1,3],[2,2,3,1]
 verticalM30x40 = [[3,2],[2,3,1,1],[1,3,1,2,1],[3,1,1,2,2,5],[2,2,1,2,1,9],[2,1,2,2,6,4],[2,2,2,1,1,7,2],[1,3,2,2,9,1],[1,3,2,1,1,10],[1,2,2,1,1,1,4,6],[1,2,2,3,3,5,5],[1,2,2,2,1,1,2,7,4],[1,3,3,2,1,2,9,4],[2,2,5,3,12,3],[2,3,9,3,8,2],[1,2,19,6,2],[2,3,18,5,1],[1,3,9,6,5,1],[2,4,6,5,4,1],[2,6,4,4,4],[5,10,4,3,3],[12,4,3,3,3],[2,10,3,3,3,3],[2,2,9,3,2,3,4],[1,2,6,3,3,3,2,1],[3,1,5,3,2,3,2,1],[1,2,1,4,3,1,3,2,1],[2,1,1,2,2,5,2,2,1],[4,1,2,3,3,5,3,2,2],[4,1,2,6,2,2,3,2,2]]
 horizontalM30x40 = [[3,10,2,1,2],[2,4,4,2,1,3],[1,3,4,3,1,1,3],[3,9,2,2,2],[3,5,5,2,1,2],[2,2,4,4,1,2,2],[4,3,1,2],[4,2,2,3],[3,1,2,2],[5,3,2,1],[4,3,1,2],[2,6,4],[1,1,3,3,5],[4,1,3,5],[2,3,1,6,5,2],[1,5,4,4],[2,3,4,2],[2,1,3,5,1],[1,3,5,1],[4,1,4,2],[4,4,2,7],[2,2,5,1,5],[3,6,2,3,1],[4,2,3,5],[1,6,3,2],[2,2,3,2,7],[3,3,4,2,6],[9,4,3,4],[11,4,4],[11,4,4],[12,4,3,1],[6,6,5,2],[2,4,6,9],[2,4,6,7],[3,4,6,3,1],[2,5,6,3],[3,5,14],[2,5,10,1],[1,6,5,2],[7,6]]
 
-def createImg(genes, width, height):
+def createImg(genes, height, width):
     img = np.zeros((height, width))
     index = 0
     for i in range(height):
@@ -46,19 +46,6 @@ def gradeFunc(solution, pattern):
             grade -= pattern[i]
 
     return grade
-    # return [grade, filteredRowPatern, pattern]
-
-# #correct
-# print(gradeFunc([0,1,1,1,0,0,0,1,0,1,1], [3,1,2]))
-# print(gradeFunc([0,1,1,1,0,1,0,0,0,1,1], [3,1,2]))
-# print(gradeFunc([1,1,1,0,0,0,0,1,0,1,1], [3,1,2]))
-# #zła kolejność bloków
-# print(gradeFunc([0,1,1,1,0,0,1,1,0,0,1], [3,1,2]))
-# #złe
-# print(gradeFunc([0,1,0,1,0,0,0,1,0,1,1], [3,1,2]))
-# print(gradeFunc([0,1,1,1,1,0,0,1,0,1,1], [3,1,2]))
-# print(gradeFunc([0,1,1,1,1,0,0,0,0,1,1], [3,1,2]))
-# print(gradeFunc([0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0], [3,1,2]))
 
 def runAlgorythmV1(verticalPattern, horizontalPattern):
     height = len(verticalPattern)
@@ -92,9 +79,9 @@ def runAlgorythmV1(verticalPattern, horizontalPattern):
     keep_parents = 6
     parent_selection_type = "sss"
     crossover_type = "single_point"
+    # crossover_type = "two_points" # mniej efektywne
     mutation_type = "random"
-    mutation_percent_genes = 1
-
+    mutation_percent_genes = 100/(height * width)
     stop_criteria = "reach_0"
 
     ga_instance = pygad.GA(gene_space=gene_space,
@@ -114,24 +101,32 @@ def runAlgorythmV1(verticalPattern, horizontalPattern):
 
     genes = ga_instance.best_solution()[0]
 
-    print("rows")
+    if ga_instance.best_solution()[1] != 0:
+        print("Mistakes in paterns:")
+
+        print("rows")
+        for i in range(height):
+                row = genes[i * width: i * width + width]
+                grade = gradeFunc(row, horizontalPattern[i])
+                print(row, grade)
+
+        print("columns")
+        for i in range(width):
+            column = []
+            for j in range(height):
+                column.append(int(genes[j * height + i]))
+            grade = gradeFunc(column, verticalPattern[i])
+            print(column, grade)
+
+    img = createImg(genes, height, width)
+
     for i in range(height):
-            row = genes[i * width: i * width + width]
-            grade = gradeFunc(row, horizontalM[i])
-            print(row, grade)
-
-    print("columns")
-    for i in range(width):
-        column = []
-        for j in range(height):
-            column.append(int(genes[j * height + i]))
-        grade = gradeFunc(column, verticalPattern[i])
-        print(column, grade)
-
-    print("Generations:", ga_instance.generations_completed, "Fitness:", ga_instance.best_solution()[1])
-
-
-    img = createImg(genes, len(verticalM), len(horizontalM))
+            for j in range(width):
+                if img[i][j] == 0:
+                    print("██", end="")
+                else:
+                    print("░░", end="")
+            print("")
 
     plt.imshow(img, cmap="gray")
     ga_instance.plot_fitness()
@@ -177,13 +172,6 @@ def gradeFuncV2(solution, pattern):
             grade -= pattern[i]
 
     return grade
-
-# def gradeGenPattern(pattern):
-#     grade = 0
-#     for i in range(len(pattern) - 1): # -1 powinno zostać usunięte
-#         if pattern[i][0] + pattern[i][1] >= pattern[i+1][0]:
-#             grade -= 1000
-#     return grade
 
 def gradeGenPatternV2(pattern):
     # pattern[i][0] początek bloku
@@ -294,4 +282,4 @@ def runAlgorythmV2(verticalPattern, horizontalPattern):
     plt.show()
     return ga_instance
 
-runAlgorythmV2(verticalM10, horizontalM10)
+runAlgorythmV2(verticalM, horizontalM)

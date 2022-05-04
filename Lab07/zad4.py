7863096365f2b7727a62121a83f36c57c204204d
Paweł Olszewski <monte.paw@gmail.com>
2022-05-04 23:33:07 +0200

    Rozwiązanie zadań z lab7

--- Lab07/zad4.py
+++ Lab07/zad4.py
@@ -4,43 +4,41 @@ from sklearn import tree
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
-# A
-df = pd.read_csv("./Lab07/iris.csv") # dla Windows'a
-# df = pd.read_csv("iris.csv") # dla Linux'a
+# Import danych
+df = pd.read_csv("./Lab07/diabetes.csv") # dla Windows'a
+# df = pd.read_csv("diabetes.csv") # dla Linux'a
 
-feature_cols = ['sepallength','sepalwidth','petallength','petalwidth']
+feature_cols = ['pregnant-times','glucose-concentr','blood-pressure','skin-thickness','insulin','mass-index','pedigree-func','age']
 features = df[feature_cols]
 labels = df['class']
 
 train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.3, random_state=1)
 # print(train_features)
 
-# B
+# Inicjalizacja i trenowanie drzewa
 clf = tree.DecisionTreeClassifier()
-
-# C
 clf.fit(train_features, train_labels)
 
-# D
+# Sprawdzenie poprawności klasyfikatora
+print("Poprawność klayfikatora: ", clf.score(test_features, test_labels))
+
+# Wyświetlenie drzewa decyzyjnego w formie graficznej
 tree.plot_tree(clf)
 plt.show()
 
-# E
-print("Poprawność klayfikatora: ", clf.score(test_features, test_labels))
-
-# F
+# Wyświetlenie macierzy błędów
 class_true = []
 class_pred = clf.predict(test_features)
 
 for i in range(len(test_features)):
     class_true.append(test_labels.iloc[i])
 
-labels = ['setosa','versicolor','virginica']
+labels = ['tested_positive','tested_negative']
 print('\nConfusion matrix:\n')
 matrix = confusion_matrix(class_true, class_pred, labels=labels)
 
-print("           | %-10s | %-10s | %-10s" % (labels[0], labels[1], labels[2]))
+print("                | %-15s | %-15s" % (labels[0], labels[1]))
 print("----------------------------------------------------")
 for i in range(len(labels)):
-    print("%-10s | %6d     | %6d     | %6d" % (labels[i], matrix[i][0], matrix[i][1], matrix[i][2]))
+    print("%-15s | %8d        | %8d" % (labels[i], matrix[i][0], matrix[i][1]))
     print("----------------------------------------------------")
